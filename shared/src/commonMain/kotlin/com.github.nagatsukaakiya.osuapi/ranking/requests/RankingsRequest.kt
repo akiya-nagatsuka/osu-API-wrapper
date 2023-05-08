fd9de28a54ac80990a1e77af33f46ee9eaa62ded
Nagatsuka Akiya <nagatsuka.akiya@gmail.com>
2023-05-08 17:54:14 +0900

    Add rankings API

--- shared/src/commonMain/kotlin/com.github.nagatsukaakiya.osuapi/ranking/requests/RankingsRequest.kt
+++ shared/src/commonMain/kotlin/com.github.nagatsukaakiya.osuapi/ranking/requests/RankingsRequest.kt
@@ -3,4 +3,22 @@ package com.github.nagatsukaakiya.osuapi.ranking.requests
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class RankingsRequest(val mode: GameMode, val type: RankingType)
+data class RankingsRequest(
+    /** Filter ranking by country code. Only available for type of performance. */
+    val country: Int?,
+    /** Cursor. */
+    val cursor: String?,
+    /** Either all (default) or friends. */
+    val filter: String?,
+    /**
+     * The id of the spotlight if type is charts.
+     * Ranking for latest spotlight will be returned if not specified.
+     */
+    val spotlight: String?,
+    /**
+     * Filter ranking to specified mode variant.
+     * For mode of mania, it's either 4k or 7k.
+     * Only available for type of performance.
+     */
+    val variant: String?,
+)
---
+++ shared/src/commonMain/kotlin/com.github.nagatsukaakiya.osuapi/ranking/response/Spotlights.kt
@@ -0,0 +1,6 @@
+package com.github.nagatsukaakiya.osuapi.ranking.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Spotlights(val spotlights: List<Spotlight>)
